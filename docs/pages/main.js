bb16c03774950eb31e33b36c32808ce4f1798506
Hannah <cc20010630@gmail.com>
2025-04-30 18:16:51 +0800

    head引擎優化

--- docs/pages/main.js
+++ docs/pages/main.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
     const loader = document.getElementById('loader');
     setTimeout(() => {
       loader.classList.add('fade-out');
-    }, 3000); 
+    }, 2000); 
   });
   
   
